be2f1c72ddc7f740dd670a90f53468492c95418f
unknown <maxxsarcilla@gmail.com>
2024-06-21 14:07:00 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 25, 75, 10 };
+
+        int highest = numbers[0];
+        int lowest = numbers[0];
+
+        for (int i = 1; i < numbers.Length; i++)
+        {
+            if (numbers[i] > highest)
+            {
+                highest = numbers[i];
+            }
+            if (numbers[i] < lowest)
+            {
+                lowest = numbers[i];
+            }
+        }
+
+        Console.WriteLine($"The Highest Number: {highest}");
+        Console.WriteLine($"The Lowest Number: {lowest}");
+    }
+}
+
